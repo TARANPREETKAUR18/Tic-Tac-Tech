2e5174cfe4311671b7d89b6412da2288dfc99728
Taranpreet Kaur <101043541+TARANPREETKAUR18@users.noreply.github.com>
2024-01-08 22:56:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset-btn");
+let hiddenText = document.querySelector(".hidden-text");
+
+let turnO = true; // turn of playerO
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [6, 7, 8],
+];
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    hiddenText.classList.add("hide");
+    resetBtn.innerText = "Reset";
+};
+
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+const showWinner = (winner) => {
+    resetBtn.innerText = `${winner} wins`;
+    hiddenText.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let val1 = boxes[pattern[0]].innerText;
+        let val2 = boxes[pattern[1]].innerText;
+        let val3 = boxes[pattern[2]].innerText;
+
+        if(val1 !== "" && val2 !== "" && val3 !==""){
+            if(val1 === val2 && val2 === val3){
+                showWinner(val1);
+            }
+        }
+    }
+};
+
+const audioX = new Audio("cross-click.mp3");
+const audioY = new Audio("o-click.wav");
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turnO){
+            box.innerText = "o";
+            turnO = false;
+            audioY.play();
+        }
+        else{
+            box.innerText = "x";
+            turnO = true;
+            audioX.play();
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+resetBtn.addEventListener("click", resetGame);
